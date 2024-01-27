66375d35fb5baf84d79d680c2762ec0951f98d33
Lucas Malandrino <lucas.malandrino@gmail.com>
2024-01-27 20:15:41 +0100

    feat: VM nixos config

--- 1-nixos/default.nix
+++ 1-nixos/default.nix
@@ -1,5 +1,27 @@
-{inputs, ...}: let
+{
+  inputs,
+  self,
+  ...
+}: let
   lib = inputs.nixpkgs.lib;
 in {
-  flake.nixosConfigurations.vm = lib.nixosSystem {};
+  imports = [./vm.nix];
+
+  flake.nixosModules.bootloaderSystemd = {...}: {
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+  };
+
+  flake.nixosModules.userLucas = {...}: {
+    services.openssh.enable = true;
+
+    users.users."lucas" = {
+      isNormalUser = true;
+      extraGroups = ["wheel"];
+
+      openssh.authorizedKeys.keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCrztDvElHynPqE3LQqZ0qjm4uaodWsbc4UUQ2v37ZlfGRZX5LX38/J+B9YmhbUMl7u41ISmqxJy3W1KyEz7IlBhwQN2TYd7zzAXJkyFd4K26MVzMlWNrFq7lF5UykHbilnOTJYwkenUZBOuxrnxXaSZ44J9LC+CVC1oQ0QevHRW/FsFLpeIn2d8eUp8VGp/Gb5++ei7Uk1lTTB4lyApGWRcebaTp+cXPAB3qar9a9f5BPL21vo8BRQYf8d0SjgqgHpwsW7tA6kIcS3QWAVqap23j4tSAou5Atqy5H1K8oFnLRMfvieqAhAdZ9U4ZBV9A0Z4KRBV0bTYYER2cv9BGkVL/hQFS1BfBfRHAinUBB5QPaG/hdvZn2UMg894vIE9kiJ+N79QmoonsGe6vD39skOlhwfNfA6NIvPhbpWXZGuqgBCx7BlCz2WlUwesuxBSRBMaFMcYoGB4b5k3xjPPs8KbBWTwlZymFvIOk3EZvaUOiryNpR9zo2kM5xRDcKXovU="
+      ];
+    };
+  };
 }
---
+++ 1-nixos/vm.nix
@@ -0,0 +1,88 @@
+{
+  inputs,
+  self,
+  ...
+}: let
+  lib = inputs.nixpkgs.lib;
+in {
+  flake = {
+    nixosConfigurations.vm = lib.nixosSystem {
+      modules = with self.nixosModules; [
+        vmHardwareConfiguration
+        vmFilesystems
+        bootloaderSystemd
+        userLucas
+        vm
+      ];
+    };
+
+    nixosModules.vm = {...}: {
+      networking.hostName = "vm";
+      networking.networkmanager.enable = true;
+
+      time.timeZone = "Europe/Paris";
+
+      i18n.defaultLocale = "en_US.UTF-8";
+      console.keyMap = "fr";
+    };
+
+    nixosModules.vmHardwareConfiguration = {
+      lib,
+      modulesPath,
+      ...
+    }: {
+      imports = [(modulesPath + "/profiles/qemu-guest.nix")];
+
+      boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod"];
+      boot.initrd.kernelModules = [];
+      boot.kernelModules = ["kvm-intel"];
+      boot.extraModulePackages = [];
+
+      networking.useDHCP = lib.mkDefault true;
+
+      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+      system.stateVersion = "23.11";
+    };
+
+    nixosModules.vmFilesystems = {...}: let
+      diskByLabel = label: "/dev/disk/by-label/${label}";
+    in {
+      fileSystems."/boot" = {
+        device = diskByLabel "bootloader";
+        fsType = "vfat";
+      };
+
+      fileSystems."/" = {
+        device = diskByLabel "nixos";
+        fsType = "btrfs";
+        options = ["subvol=@"];
+      };
+
+      fileSystems."/nix" = {
+        device = diskByLabel "nixos";
+        fsType = "btrfs";
+        options = ["subvol=@nix"];
+      };
+
+      fileSystems."/var/log" = {
+        device = diskByLabel "nixos";
+        fsType = "btrfs";
+        options = ["subvol=@log"];
+      };
+
+      fileSystems."/home" = {
+        device = diskByLabel "nixos";
+        fsType = "btrfs";
+        options = ["subvol=@home"];
+      };
+
+      fileSystems."/root" = {
+        device = diskByLabel "nixos";
+        fsType = "btrfs";
+        options = ["subvol=@root"];
+      };
+
+      swapDevices = [];
+    };
+  };
+}

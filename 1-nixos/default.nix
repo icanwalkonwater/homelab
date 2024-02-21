f04224e1bc2f703f93179baab02d0ec38df2c4f5
Lucas Malandrino <lucas.malandrino@gmail.com>
2024-02-21 23:55:49 +0100

    fix: Reenable stuff

--- 1-nixos/default.nix
+++ 1-nixos/default.nix
@@ -45,12 +45,12 @@
         role = "server";
         clusterInit = true;
         extraFlags = toString [
-          # "--secrets-encryption"
+          "--secrets-encryption"
           # "--cluster-cidr=10.42.0.0/16,2001:cafe:42::/56"
           # "--service-cidr=10.43.0.0/16,2001:cafe:43::/112"
-          # "--flannel-backend=wireguard-native"
+          "--flannel-backend=wireguard-native"
           "--disable-cloud-controller"
-          # "--disable=traefik"
+          "--disable=traefik"
         ];
       };
 
